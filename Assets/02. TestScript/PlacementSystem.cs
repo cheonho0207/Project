674aa44ead99c943ecf2efcbeb606618cad2b3f6
cheonho0207 <hyoonjk2@naver.com>
2024-01-20 19:47:20 +0900

    01/20 div

--- Assets/02. TestScript/PlacementSystem.cs
+++ Assets/02. TestScript/PlacementSystem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,12 +11,68 @@ public class PlacementSystem : MonoBehaviour
     private InputManagerment inputManager;
     [SerializeField]
     private Grid grid;
+    [SerializeField]
+    private GameObject Stoptext;
 
+    [SerializeField]
+    private ObjectsDatabaseSO database;
+    private int selectedObjectIndex;
+
+    [SerializeField]
+    //private GameObject gridVisualization;
+
+    
+    private void Start()
+    {
+        StopPlacement();
+    }
+
+    public void StartPlacement(int ID)
+    {
+        StopPlacement();
+        selectedObjectIndex = database.objectsData.FindIndex(data => data.ID == ID);
+        if (selectedObjectIndex < 0)
+        {
+            Debug.LogError($"No ID foudn {ID}");
+            return;
+        }
+        //gridVisualization.SetActive(true);
+        cellIndicator.SetActive(true);
+        Stoptext.SetActive(true);
+        inputManager.OnClicked += PlaceStructure;
+        inputManager.OnExit += StopPlacement;
+    }
+
+    private void PlaceStructure()
+    {
+        //if(inputManager.IsPointerOverUI())
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+        GameObject newObject = Instantiate(database.objectsData[selectedObjectIndex].Prefab);
+        newObject.transform.position = grid.CellToWorld(gridPosition);
+        StopPlacement();
+    }
+
+    private void StopPlacement()
+    {
+        selectedObjectIndex = -1;
+        //gridVisualization.SetActive(false);
+        cellIndicator.SetActive(false);
+        Stoptext.SetActive(false);
+        inputManager.OnClicked -= PlaceStructure;
+        inputManager.OnExit -= StopPlacement;
+    }
+    
     private void Update()
     {
+        
+        if (selectedObjectIndex < 0)
+            return;
+        
         Vector3 mousePosition = inputManager.GetSelectedMapPosition();
-        Vector3Int gridPosition=grid.WorldToCell(mousePosition);
-        mouseIndicator.transform.position = mousePosition;
-        cellIndicator.transform.position = grid.CellToWorld(gridPosition);
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+        mouseIndicator.transform.position = mousePosition; //mouse point
+        cellIndicator.transform.position = grid.CellToWorld(gridPosition); //show pointed sell
     }
 }
+    
