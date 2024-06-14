99813d0bdadedfdb16912ec0e0613b57e4e55528
cheonho0207 <hyoonjk2@naver.com>
2024-06-14 19:28:33 +0900

    update

---
+++ Assets/02. TestScript/UISystem/HPManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class HPManager : MonoBehaviour
+{
+    public Image HpBar;
+    public static readonly float initHP = 100.0f;
+    public static float CurrHP = initHP;
+
+    void Awake()
+    {
+        HpBar = GameObject.FindGameObjectWithTag("HP")?.GetComponent<Image>();
+        CurrHP = initHP;
+    }
+
+    public void HpDown()
+    {
+        if (CurrHP > 0.0f)
+        {
+            CurrHP -= 10;
+            DisplayHealth();
+            if (CurrHP <= 0.0f)
+            {
+                CurrHP = 10;
+                DisplayHealth();
+                //SceneManager.LoadScene("LoseScene");
+            }
+        }
+    }
+
+    void DisplayHealth() //HP 상태를 보여줌
+    {
+        if (HpBar != null)
+        {
+            HpBar.fillAmount = CurrHP / initHP; //UI의 HP 계산
+        }
+        else
+        {
+            Debug.LogError("Unity Editor에서 HpBar가 할당되지 않았습니다.");
+        }
+    }
+}
